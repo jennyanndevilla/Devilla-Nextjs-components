6d2bc275916f239f0d360cb422a8d53bb7c1cadd
jennyanndevilla <jennyann.devilla@hcdc.edu.ph>
2025-10-03 22:16:33 +0800

    Create ProfileCard.tsx

---
+++ ProfileCard.tsx
@@ -0,0 +1,10 @@
+export default function ProfileCard() {
+  return (
+    <div className="profile-card">
+      <img src="/jennyimage.jpg" alt="Profile" />
+      <h2>Jenny Ann S. Devilla</h2>
+      <p>Age: 19</p>
+      <p> I like watching movies also play online games and I like sleelping  </p>
+    </div>
+  );
+}
