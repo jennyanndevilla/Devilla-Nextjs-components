6621e1ee88174a7194a1463e9d5c847f7c88594c
jennyanndevilla <jennyann.devilla@hcdc.edu.ph>
2025-10-03 22:17:17 +0800

    Create SkillsList.tsx

---
+++ SkillsList.tsx
@@ -0,0 +1,12 @@
+export default function SkillsList() {
+  return (
+    <div className="skills">
+      <h3>Skills</h3>
+      <ul>
+        <li>Writing</li>
+        <li>Multitasking</li>
+        <li>Editing</li>
+      </ul>
+    </div>
+  );
+}
